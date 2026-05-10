e83c1d9dbd3c46c655bb27e1301fea14f93aa614
김용석 <76533032+YSbookcase@users.noreply.github.com>
2026-05-10 20:11:08 +0900

    [level 0] Title: 홀짝 구분하기, Time: 16.37 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String[] s;
+
+        Console.Clear();
+        s = Console.ReadLine().Split(' ');
+
+        int a = Int32.Parse(s[0]);
+        
+        if(a%2 == 0)
+        {
+            Console.WriteLine($"{a} is even");
+        }
+        else if(a%2 != 0)
+        {
+            Console.WriteLine($"{a} is odd");
+        }
+        else
+        {
+            Console.WriteLine("입력에 문제가 있는듯 싶다.");
+        }
+    }
+}
