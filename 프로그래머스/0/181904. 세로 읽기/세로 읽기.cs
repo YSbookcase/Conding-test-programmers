550ba6b689d02d016c6e1abfeb1a9155e62036b7
김용석 <76533032+YSbookcase@users.noreply.github.com>
2026-08-17 02:26:25 +0900

    [level 0] Title: 세로 읽기, Time: 0.17 ms, Memory: 46 MB -BaekjoonHub

---
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Text;
+
+public class Solution {
+    public string solution(string my_string, int m, int c) {
+       
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = c-1; i < my_string.Length; i += m)
+        {
+            sb.Append(my_string[i]);
+        }
+        
+        return sb.ToString();
+    }
+}
