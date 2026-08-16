ff38a3cb9c1d7ffd8ba45bd0d4ecedb6a3d2d02a
김용석 <76533032+YSbookcase@users.noreply.github.com>
2026-08-17 00:38:20 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.08 ms, Memory: 46.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string, int n) {
+       
+        string result = my_string.Substring(0, n);
+            
+            return result;
+    }
+}
