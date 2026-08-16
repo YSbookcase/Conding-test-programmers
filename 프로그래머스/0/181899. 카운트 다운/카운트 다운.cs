3fc1bca87e73076788369a09e312838d62dff0db
김용석 <76533032+YSbookcase@users.noreply.github.com>
2026-08-17 05:42:01 +0900

    [level 0] Title: 카운트 다운, Time: 0.12 ms, Memory: 46.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = new int[start_num - end_num +1];
+        int index = 0;
+        
+        for(int i = start_num; i >= end_num; i--)
+        {
+            answer[index] = i;
+            index++;
+        }
+        
+        return answer;
+        
+    }
+}
