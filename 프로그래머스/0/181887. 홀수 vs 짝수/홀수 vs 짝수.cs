e0f7ec3d1ded62966c19cb2fed4e949a8db6c480
김용석 <76533032+YSbookcase@users.noreply.github.com>
2026-08-19 22:08:25 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.14 ms, Memory: 46.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.cs
@@ -0,0 +1,38 @@
+using System;
+
+public class Solution {
+    public int solution(int[] num_list) {
+        int oddSum = 0;
+        int evenSum = 0;
+        
+//         for(int i = 0; i < num_list.Length; i++)
+//         {
+//             if((i+1)%2 != 0)
+//             {
+//                 oddSum += num_list[i];
+//             }
+//             else if( (i+1)%2 == 0)
+//             {
+//                 evenSum += num_list[i];
+//             }
+//         }
+        
+//         return oddSum > evenSum ? oddSum : evenSum;
+//     }
+        
+        for(int i = 0; i < num_list.Length; i++)
+        {
+            if(i % 2 == 0)
+            {
+                oddSum += num_list[i];
+            }
+            else
+            {
+                evenSum += num_list[i];
+            }
+        }
+        
+        return Math.Max(evenSum, oddSum);
+    }
+        
+}
