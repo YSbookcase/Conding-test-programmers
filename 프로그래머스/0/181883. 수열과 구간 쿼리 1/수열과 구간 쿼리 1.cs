07990d32dcb848d81cec19b3fde3806f53f97f24
김용석 <76533032+YSbookcase@users.noreply.github.com>
2026-08-20 07:34:32 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 0.31 ms, Memory: 46.9 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.cs
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.cs
@@ -5,18 +5,38 @@ public class Solution {
         
         
             
-            for(int i = 0; i < queries.GetLength(0); i++)
-            {
+//             for(int i = 0; i < queries.GetLength(0); i++)
+//             {
                
-                    int start = queries[i,0];
-                    int end = queries[i,1];
+//                     int start = queries[i,0];
+//                     int end = queries[i,1];
                     
-                    for(int a = start; a <= end; a++)
-                    {
-                        arr[a] += 1;
-                    }
+//                     for(int a = start; a <= end; a++)
+//                     {
+//                         arr[a] += 1;
+//                     }
                 
-            }
+//             }
+        
+        int[] diff = new int[arr.Length + 1];
+        
+        for(int i = 0; i <queries.GetLength(0); i++)
+        {
+            int start = queries[i, 0];
+            int end = queries[i, 1];
+            
+            diff[start] += 1;
+            diff[end + 1] -= 1;
+        }        
+
+        int add = 0;
+        
+        for(int i = 0; i < arr.Length; i++)
+        {
+            add += diff[i];
+            arr[i] += add;
+        }
+        
         
         return arr;
     }
