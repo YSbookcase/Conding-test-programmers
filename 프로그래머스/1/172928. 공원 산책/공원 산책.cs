0a95a8a96bc1fb2f3a561ec1eb039ccedd074cf6
김용석 <76533032+YSbookcase@users.noreply.github.com>
2026-09-16 23:55:50 +0900

    [level 1] Title: 공원 산책, Time: 1.13 ms, Memory: 48.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.cs
@@ -0,0 +1,85 @@
+using System;
+
+public class Solution {
+    public int[] solution(string[] park, string[] routes) {
+            int height = park.Length;
+     int width = park[0].Length;
+
+     int row = 0;
+     int col = 0;
+     for (int r = 0; r < height; r++)
+     {
+         for (int c = 0; c < width; c++)
+         {
+             if (park[r][c] == 'S')
+             {
+                 row = r;
+                 col = c;
+             }
+         }
+     }
+
+     foreach (string route in routes)
+     {
+         string[] parts = route.Split(' ');
+         string op = parts[0];
+         int step = int.Parse(parts[1]);
+
+         int dRow = 0;
+         int dCol = 0;
+
+         switch (op)
+         {
+             case "N":
+                 dRow = -1;
+                 break;
+             case "S":
+                 dRow = 1;
+                 break;
+             case "W":
+                 dCol = -1;
+                 break;
+             case "E":
+                 dCol = 1;
+                 break;
+
+         }
+
+         int nextRow = row;
+         int nextCol = col;
+         bool canMove = true;
+
+         for (int i = 0; i < step; i++)
+         {
+             nextRow += dRow;
+             nextCol += dCol;
+
+             if (nextRow < 0 || nextRow >= height || nextCol < 0 || nextCol >= width)
+             {
+
+                 canMove = false;
+                 break;
+
+             }
+
+             if (park[nextRow][nextCol] == 'X')
+             {
+                 canMove = false;
+                 break;
+             }
+
+         
+         }
+         
+             if (canMove)
+             {
+                 row = nextRow;
+                 col = nextCol;
+             }
+     }
+
+     return new int[] { row, col };
+ }
+
+    
+}
